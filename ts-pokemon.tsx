1e485c4ad8d1d08ac686c574388cd90b7e6a2ccf
samuel21695 <samuel21695@gmail.com>
2023-12-19 20:56:34 +0900

    📄 tsx format

---
+++ ts-pokemon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const PokemonList: React.FC = () => {
+  const pokemon = ["피카츄", "라이츄", "파이리", "꼬부기"];
+
+  const updatedPokemon: string[] = pokemon.map((pokemonName) => {
+    if (pokemonName === "파이리") {
+      return "버터풀";
+    }
+    return pokemonName;
+  });
+
+  const pokemonElements = updatedPokemon.map((pokemonName, index) => (
+    <div key={index}>
+      <p>{pokemonName}</p>
+    </div>
+  ));
+
+    return <div>{pokemonElements}</div>;
+};
+
+console.log(PokemonList);
+
+export default PokemonList;
